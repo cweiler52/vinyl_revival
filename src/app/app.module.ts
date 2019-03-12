9c37a47a1ace6b465f3d1f011669e7123bec9a85
Arrhe <arrhenr@gmail.com>
2019-03-12 16:04:31 -0400

    file reorder

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/database.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+//import { Plant } from '../models/plant.model'; ---> ADD PRODUCT IMPORT
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': localStorage.getItem('token')
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  private dbProductsUrl = 'https://efa-gardenapp-backend.herokuapp.com/api/product';
+  private dbLogUrl = 'http://localhost:3005/api/login';
+  private dbSignUrl = 'http://localhost:3005/api/signup';
+  
+  constructor(private http: HttpClient) { }
+
+  // getPlants() : Observable<Plant[]> {
+  //   return this.http.get<Plant[]>(this.dbProductsUrl);
+  // }
+  
+  // deletePlant(id: any) : Observable<Plant> {
+  //   const deleteProductsUrl = `${this.dbProductsUrl}/${id}`;
+  //   // console.log(deleteProductsUrl);
+  //   return this.http.delete<Plant>(deleteProductsUrl, httpOptions);
+  // }
+
+  loginUser(user) {
+    return this.http.post<any>(this.dbLogUrl, user)
+      .pipe(map(user => {
+         if (user && user.sessionToken) {
+             localStorage.setItem('token', user.sessionToken);
+         }
+         return user;
+      }));
+  }
+  
+  SignupUser(user) {
+    return this.http.post<any>(this.dbSignUrl, user)
+      .pipe(map(user => { console.log(user)
+         if (user && user.sessionToken) {
+             localStorage.setItem('token', user.sessionToken);
+         }
+         return user;
+      }));
+  }
+
+  logoutUser() {
+      localStorage.removeItem('token');
+  }
+}

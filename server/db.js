b34deb04ec55b974d9d909288236454b4b58f6fc
Cliff Weiler <cweiler52@gmail.com>
2019-03-12 15:16:15 -0400

    changing back a few files to deploy to heroku

--- server/db.js
+++ server/db.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 
-// const sequelize = new Sequelize(process.env.DBNAME, 'postgres', process.env.DBPASS, {
-//     host: 'localhost',
-//     dialect: 'postgres'
-// });
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DBNAME, 'postgres', process.env.DBPASS, {
+    host: 'localhost',
     dialect: 'postgres'
 });
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres'
+// });
 
 sequelize.authenticate().then(
     function() {

9874d10749ade57e62e9c032eb9fc4d17c787f12
Cliff Weiler <cweiler52@gmail.com>
2019-03-20 14:48:21 -0400

    adding in more authguard & navigation stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 
-// MAIN STOREFRONT COMPONENTS //////////////////////////////////////////////////////////////////////
+// MAIN STOREFRONT COMPONENTS ///////////////////////////////////////////////////////////
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
 import { ProductViewComponent } from './product-view/product-view.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-// ADMIN COMPONENTS //////////////////////////////////////////////////////////////////////
 
+// ADMIN COMPONENT //////////////////////////////////////////////////////////////////////
 import { AdminProdList } from './admin-prodlist/admin-prodlist.component';
-import { AdminUserlist } from './admin-userlist/admin-userlist.component';
 
 const routes: Routes = [
   // MAIN STOREFRONT PAGES
@@ -20,10 +19,9 @@ const routes: Routes = [
   { path: 'contact', component: AboutComponent },
   { path: 'record/:id', component: ProductViewComponent },
   // USER PROFILE PAGE
-  { path: 'user/profile', component: UserProfileComponent },
+  { path: 'user/profile', component: UserProfileComponent, canActivate: [AuthGuard], data: ['user'] },
   // ADMIN PAGES
-  { path: 'admin/products', component: AdminProdList, canActivate: [AuthGuard] },
-  { path: 'admin/users', component: AdminUserlist }
+  { path: 'admin/products', component: AdminProdList, canActivate: [AuthGuard], data: ['admin'] },
 ];
 
 @NgModule({
@@ -33,4 +31,4 @@ const routes: Routes = [
 export class AppRoutingModule { }
 // CONST RoutingComponents IS AN ARRAY OF IMPORTED COMPONENTS THAT WE CAN THEN ...
 // IMPORT INTO app.module.ts AS ONE IMPORT & SET IN THE declarations OBJECT OF @NgModule
-export const RoutingComponents = [ HomepageComponent, AboutComponent, ProductViewComponent, UserProfileComponent, AdminProdList, AdminUserlist ];
+export const RoutingComponents = [ HomepageComponent, AboutComponent, ProductViewComponent, UserProfileComponent, AdminProdList ];
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { DatabaseService } from '../database.service';
 
@@ -9,11 +9,27 @@ import { DatabaseService } from '../database.service';
 export class AuthGuard implements CanActivate {
   auth = this.dbService.getCookies();
 
-  constructor(private dbService: DatabaseService) { }
+  constructor(private dbService: DatabaseService, private router: Router) { }
 
   canActivate (
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      return this.auth.is_admin;
+      if(next.data[0] === 'user'){
+        //this.router.navigate(['/dashboard']);
+        if(!this.auth.is_admin) {
+          return true;
+        }else{
+          this.router.navigate(['/']);
+        }
+        return false;
+
+      }else if(next.data[0] === 'admin'){
+        if(this.auth.is_admin) {
+          return true;
+        }else{
+          this.router.navigate(['/']);
+        }
+        return false;
+      }
     }
 }

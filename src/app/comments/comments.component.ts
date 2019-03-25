07d360c804fabe19ecc1625bd5778f8c5e624304
Cliff Weiler <cweiler52@gmail.com>
2019-03-25 11:46:13 -0400

    still working with comments

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -56,7 +56,6 @@ export class CommentsComponent implements OnChanges {
   
   editToggle(id, text){
     //console.log(id, text)
-    // document.getElementById('comment_create').addClass('hidden');
     document.getElementById(`comment_edit_${id}`).classList.toggle('hidden');
     document.getElementById(`comment_input_edit_${id}`).value = text;
   }

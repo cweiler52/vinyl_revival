a914ed09ba221b1176dce0b824bd8d6f00c5fbef
Cliff Weiler <cweiler52@gmail.com>
2019-03-26 21:24:31 -0400

    environment issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,13 +7,11 @@ export let APIURL = '';
 switch (window.location.hostname) {
     // this is the deployed angular application
     case 'vinylrevival.herokuapp.com':
-    console.log(window.location.hostname);
-    // this is the full url of your deployed API
+        // this is the full url of your deployed API
         APIURL = 'https://vinylrevival-server.herokuapp.com';
     break;
     default:
-    console.log(window.location.hostname);
-    // this is the local host name of your API
+        // this is the local host name of your API
         APIURL = 'http://localhost:3005';
     break;
 }
